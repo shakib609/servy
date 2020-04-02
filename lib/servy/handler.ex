36bf0369c80b1383754cc943d7dfcc3845bed25c
Shakib Hossain <shakib609@gmail.com>
2020-04-03 02:54:33 +0600

    Finish L11

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -2,6 +2,10 @@ defmodule Servy.Handler do
   @moduledoc "Handles HTTP Requests"
 
   @pages_path Path.expand("../../pages", __DIR__)
+
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+  import Servy.Parser, only: [parse: 1]
+
   def handle(request) do
     request
     |> parse
@@ -10,36 +14,6 @@ defmodule Servy.Handler do
     |> route
     |> track
     |> format_response
-  end
-
-  def track(%{status: 404, path: path} = conv) do
-    IO.puts("Warning! #{path} is on the loose!")
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def log(conv), do: IO.inspect(conv)
-
-  def rewrite_path(%{path: "/wildlife"} = conv) do
-    %{conv | path: "/wildthings"}
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first()
-      |> String.split(" ")
-
-    %{
-      method: method,
-      path: path,
-      status: nil,
-      resp_body: ""
-    }
   end
 
   def route(%{method: "GET", path: "/wildthings"} = conv) do
